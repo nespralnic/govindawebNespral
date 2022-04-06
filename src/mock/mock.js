b75591c1b7fc15e006f47c88072d426f363bc927
nespralnic <niconespral@gmail.com>
2022-04-06 15:51:52 -0300

    preentrega en main no pushear todavía

---
+++ src/mock/mock.js
@@ -0,0 +1,54 @@
+import foto from '../img/falafel.JPG';
+
+export  const productosIniciales = [
+    {   
+        
+        id: 1, 
+        category:"tartas", 
+        name: "Hojas verdes", 
+        price: "500", 
+        pictureUrl:"",
+        descripcion:"Tarta de acelga. Veggie. Sin TACC.",
+        ingredientes: "Harina de garbanzo, Harina de arroz, Zanahoria, Acelga, Cebolla, Repollo, Puerro."},
+    
+    {   
+        id: 2, 
+        category:"tartas", 
+        name: "Brócoli", 
+        price: "530", 
+        pictureUrl:"",
+        descripcion: "Tarta de brócolis. Veggie. Sin TACC.",
+        ingredientes: "Harina de garbanzo, Harina de arroz, Zanahoria, Harina de Mandioca, Brócoli, Cebolla."},
+    {   
+        id: 3, category:"tartas", 
+        name: "Hongos", 
+        price: "530", 
+        pictureUrl:foto,
+        descripcion:"20 Mini-hamburguesas. Vegan. Sin TACC.",
+        ingredientes: "Garbanzos, cebolla, ajo, perejil, cilantro, limón."},
+    {   
+        id: 4, 
+        category:"burgers",
+        name: "Falafel", 
+        price: "470", 
+        pictureUrl:"/static/media/falafel.f3ce29d7612ca1606b1e.JPG",
+        descripcion:"20 Mini-hamburguesas. Vegan. Sin TACC.",
+        ingredientes: "Garbanzos, cebolla, ajo, perejil, cilantro, limón."
+    },
+    {
+        id: 5, 
+        category:"burgers", 
+        name: "Trigo Sarraceno", 
+        price: "500", 
+        pictureUrl:foto,
+        descripcion:"20 Mini-hamburguesas. Vegan. Sin TACC.",
+        ingredientes: "Garbanzos, cebolla, ajo, perejil, cilantro, limón."},
+    {
+        id: 6, 
+        category:"burgers", 
+        name: "Albóndigas", 
+        price: "470", 
+        pictureUrl:foto,
+        descripcion:"20 Mini-hamburguesas. Vegan. Sin TACC.",
+        ingredientes: "Garbanzos, cebolla, ajo, perejil, cilantro, limón."}
+]
